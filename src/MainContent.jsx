7b0ff3ecc808c374cc27246215bdca3b78fd74d8
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-09-16 00:08:52 +0200

    End of chapter 3

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
-  state = { pageTitle: "Customers", customersCount: 5 };
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+    customers: [
+      {
+        id: 1,
+        name: "Scott",
+        phone: "123-345",
+        address: { city: "New Delhi" },
+        photo: "https://picsum.photos/id/1010/60",
+      },
+      {
+        id: 2,
+        name: "Yes",
+        phone: "123-235",
+        address: { city: "New Jersy" },
+        photo: "https://picsum.photos/id/1011/60",
+      },
+      {
+        id: 3,
+        name: "No",
+        phone: "1222-34545",
+        address: { city: "New London" },
+        photo: "https://picsum.photos/id/1012/60",
+      },
+      {
+        id: 4,
+        name: "Maybe",
+        phone: null,
+        address: { city: "Berlin" },
+        photo: "https://picsum.photos/id/1013/60",
+      },
+      {
+        id: 5,
+        name: "Mhmm",
+        phone: null,
+        address: { city: "New York" },
+        photo: "https://picsum.photos/id/1014/60",
+      },
+    ],
+  };
 
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">
+        <h4 className="m-1 p-1">
           {this.state.pageTitle}
 
           <span className="badge badge-secondary m-2">
@@ -17,12 +57,63 @@ export default class MainContent extends Component {
             Refresh
           </button>
         </h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Photo</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>{this.getCustomerRow()}</tbody>
+        </table>
       </div>
     );
   }
 
   //Executes when the user clicks on Refresh button
-  onRefreshClick() {
-    console.log("Refresh clicked");
-  }
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 7,
+    });
+  };
+  getPhoneToRender = (phone) => {
+    if (phone) return phone;
+    else return <div className="bg-warning p-2 text-center">No phone</div>;
+  };
+
+  getCustomerRow = () => {
+    return this.state.customers.map((cust, index) => {
+      return (
+        <tr key={cust.id}>
+          <td>{cust.id}</td>
+          <td>
+            <img src={cust.photo} alt="Customer" />
+            <div>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  this.onChangePictureClick(cust, index);
+                }}
+              >
+                Change Picture
+              </button>
+            </div>
+          </td>
+          <td>{cust.name}</td>
+          <td>{this.getPhoneToRender(cust.phone)}</td>
+          <td>{cust.address.city}</td>
+        </tr>
+      );
+    });
+  };
+  onChangePictureClick = (cust, index) => {
+    // console.log(cust);
+    // console.log(index);
+    var custArr = this.state.customers;
+    custArr[index].photo = "https://picsum.photos/id/104/60";
+    this.setState({ customers: custArr });
+  };
 }
